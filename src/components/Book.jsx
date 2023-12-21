9e1adefbdc42b0d39cb3ecdc4e94e46e1b5f5645
Kavin0M <kavinsai88@gmail.com>
2023-12-21 15:58:03 +0530

    Fixed the bug

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -2,7 +2,7 @@ function Book(props) {
     if (props.data){
         return (
             <div className="book">
-                <img src={props.data.imageLinks.smallThumbnail} alt="" className="book-img"/>
+                {props.data.imageLinks ? <img src={props.data.imageLinks.smallThumbnail} alt="" className="book-img"/> : <img src="https://t4.ftcdn.net/jpg/04/00/24/31/360_F_400243185_BOxON3h9avMUX10RsDkt3pJ8iQx72kS3.jpg" alt="" className="book-img"/>}
                 <div className="title">{props.data.title}</div>
                 <div className="subtitle"><span className="rating">{props.data.averageRating? props.data.averageRating : (Math.random() * (5 - 3) + 3).toFixed(1)} &#9733;</span><span className="cost">Free</span></div>
             </div>
